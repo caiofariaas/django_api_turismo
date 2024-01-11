acf0b66ec03ec936413f9f34d29fe9b79a873ea8
caiofariaas <caiofarias077@gmail.com>
2024-01-11 08:31:14 -0300

    actions

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -2,6 +2,8 @@ from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
 from rest_framework.response import Response
+from rest_framework.decorators import action
+
 
 class PontoTuristicoViewSets(ModelViewSet):
     
@@ -10,6 +12,8 @@ class PontoTuristicoViewSets(ModelViewSet):
     def get_queryset(self):
         
         return PontoTuristico.objects.filter(aprovado=True)
+    
+    # SOBRESCRITA DE MÉTODOS PADRÕES
     
     # oque é retornado quando fazemos um GET na API
     # Podemos manipular os resultados sobrescrevendo o método list()
@@ -32,10 +36,30 @@ class PontoTuristicoViewSets(ModelViewSet):
     # retrieve
     # funciona para um GET de um recurso especifico, exemplo: '/pontoturistico/4'
     
-    def retrieve(self, request, *args, **kwargs):
+    # def retrieve(self, request, *args, **kwargs):
+    #     pass
+    
+    # # update
+    
+    # def update(self, request, *args, **kwargs):
+    #     pass
+    
+    # patch / partial_update
+    
+    # def partial_update(self, request, *args, **kwargs):
+    #     pass
+    
+    # ACTIONS
+    # método para caso deseje denunciar um ponto turistico
+    # para um recurso especifico '/pontoturistico/4/denunciar'
+    
+    @action(methods=['get'], detail=True)
+    def denunciar(self, request, pk=None):
         pass
     
-    # update
+    # Para o endpoint inteiro
+    # seria possivel fazer uma função para deletar todos os itens de um endpoint
     
-    def update(self, request, *args, **kwargs):
+    @action(methods=['get'], detail=False)
+    def teste(self, request):
         pass
